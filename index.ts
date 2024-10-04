e439d982bc2678171c7999ae8f73f1bd297adbc3
maxHyeon <maxhyeon.park@gmail.com>
2024-10-04 23:13:26 +0900

    refactor: updatetile function, stoney,boxy statement logic merge

--- index.ts
+++ index.ts
@@ -945,12 +945,11 @@ function updateMap() {
 
 function updateTile(x: number, y: number) {
   if ((map[y][x].isStoney())
+    && map[y + 1][x].isAir()
+    || (map[y][x].isBoxy())
     && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new Stone(new Falling());
-    map[y][x] = new Air();
-  } else if ((map[y][x].isBoxy())
-    && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new Box(new Falling());
+    map[y][x].drop();
+    map[y + 1][x] = map[y][x];
     map[y][x] = new Air();
   } else if (map[y][x].isFalling()) {
     map[y][x].rest();
